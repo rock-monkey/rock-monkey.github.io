70e8a93bbbd4393394790f264eb4dd411de66ae5
Dan Q <dan@danq.me>
2017-11-25 17:31:36 +0100

    Rebuild.

--- javascripts/site.js
+++ javascripts/site.js
@@ -8,9 +8,25 @@ document.querySelectorAll('.randomlink').forEach(r => {
   r.innerHTML = links[Math.floor(Math.random() * links.length)].outerHTML;
 })
 
+// Random page picker
+document.querySelector('.random-page').addEventListener('click', (e)=>{
+  e.preventDefault();
+  fetch('/tag-index.json').then(res=>res.json().then(tags=>{
+    const random_tag = tags[Math.floor(Math.random() * tags.length)];
+    window.location.href = '/' + random_tag;
+  }));
+})
+
 // Raw source viewer
 document.querySelector('.show-raw').addEventListener('click', (e)=>{
   e.preventDefault();
   document.querySelector('.show-raw').innerHTML = '';
   document.querySelector('article section').innerHTML = '<pre style="white-space: pre-wrap;">' + document.getElementById('raw').innerText + '</pre>';
+});
+
+// Wanted pages list
+document.querySelectorAll('#wanted-pages').forEach(ul => {
+  fetch ('/missing-pages.json').then(res=>res.json().then(tags=>{
+    ul.innerHTML = tags.map(tag => `<li><a href="/${tag}" class="internal broken">${tag}</a></li>`).join('')
+  }));
 });
